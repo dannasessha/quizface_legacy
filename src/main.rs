0d44272b9fe569b5805d14f142c17c05838938f3
zancas <zancas@zingolabs.com>
2020-11-26 19:10:35 -0700

    add test of documentation-vs-response validity
    In particular responses should never contain
    undocumented fields.  In this instance as
    noted by gygaxis@zingolabs.com, a field exists
    in the actual response that's not in the
    help output.
    
    This change also incorporates a test fixture
    which should make writing tests require less
    boilerplate.
    
    Notes:
    
      * the class of tests that validates the
    documentation will eventually be it's own
    mod and likely will be in the tests directory
    as it's properly integration testing
      * the machinery implemented concretely here
    can (and soon will) be easily abstracted to
    test more RPCs than just getinfo (which is
    serving as out POC RPC.
      * the overall design is to have many small
    specific tests to ease the identification of
    specific kinds of failures (as opposed to
    "omnibus" tests with many assertions that
    cover logically disparate aspects of the code
    under test (obviously this is a continuum)

--- src/main.rs
+++ src/main.rs
@@ -199,11 +199,58 @@ mod test {
     }
     mod getinfo {
         use super::*;
+        use serde_json::Value;
+        use std::collections::HashSet;
+        use std::process::Command;
+        struct GetInfoResponseFixture {
+            repr_bytes: Vec<u8>,
+            repr_string: String,
+            repr_json: Value,
+            repr_keyset: HashSet<String>,
+        }
+        impl GetInfoResponseFixture {
+            fn new() -> GetInfoResponseFixture {
+                let repr_bytes = Command::new("zcash-cli")
+                    .arg("getinfo")
+                    .output()
+                    .unwrap()
+                    .stdout;
+                let repr_string = String::from_utf8(repr_bytes.clone()).unwrap();
+                let repr_json = serde_json::de::from_str(&repr_string).unwrap();
+                let repr_keyset;
+                if let Value::Object(objmap) = &repr_json {
+                    repr_keyset = objmap.keys().cloned().collect();
+                } else {
+                    panic!()
+                }
+                GetInfoResponseFixture {
+                    repr_bytes,
+                    repr_string,
+                    repr_json,
+                    repr_keyset,
+                }
+            }
+        }
         #[test]
         fn concrete_annotation_match() {
             let static_test_annotation = fake_parse_getinfo();
             let eventually_real = fake_parse_getinfo();
             assert_eq!(static_test_annotation, eventually_real);
+        }
+        #[test]
+        fn validate_response_as_subset() {
+            let response_fixture = GetInfoResponseFixture::new();
+            let testdata_keys: HashSet<String> = fake_parse_getinfo()
+                .keys()
+                .map(|&x| x.to_string())
+                .collect();
+            dbg!(&response_fixture.repr_keyset.difference(&testdata_keys));
+            assert!(response_fixture
+                .repr_keyset
+                .difference(&testdata_keys)
+                .cloned()
+                .collect::<String>()
+                .is_empty());
         }
     }
 }
