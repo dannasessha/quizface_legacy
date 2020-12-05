126dae8665ae4576cfb689a2c9dfd0ccdf1d5f15
dannasessha <sessha@zingolabs.com>
2020-12-05 08:08:20 +0000

    refactored lib.rs for API-style, naming variables, defensiveness, and clarity

--- src/lib.rs
+++ src/lib.rs
@@ -84,16 +84,17 @@ pub fn parse_raw_output(raw_command_help: String) -> HashMap<String, String> {
 
     // for 'well formed' command help outputs (such as getinfo):
     // the relevant fields are in between `{` and `}`, assumed
-    // to be alone on a line
+    // to be alone on a line, after 'Result:' and before 'Examples:'
+    let mut beginresult: bool = false;
+    let mut beginexamples: bool = false;
     let mut start: bool = false;
     let mut end: bool = false;
-    let mut beginresult = false;
-    let mut beginexample = false;
 
     // TODO create recursive function
     for li in command_help_lines_iter {
         // TODO add helper function
         if li == "Examples:"{
+            beginexamples = true;
             break
         }
 
@@ -117,10 +118,13 @@ pub fn parse_raw_output(raw_command_help: String) -> HashMap<String, String> {
         if li == "{" && beginresult {
             start = !start;
         }
-
         
     }
 
+    if !beginexamples {
+        println!("WARNING! No examples!")
+    }
+    
     if start && !end {
         panic!("curly braces not well formed! start with no end");
     }
@@ -135,98 +139,61 @@ pub fn parse_raw_output(raw_command_help: String) -> HashMap<String, String> {
 }
 
 pub fn annotate_identifier(ident_with_metadata: String) -> (String, String) {
-    // find key. begin by selecting first str before
-    // whitespace and eliminating leading whitespace.
-    let ident = &ident_with_metadata.split('"').collect::<Vec<&str>>()[1]
-        .trim_matches('"');
-
-    let mut temp_iter = ident_with_metadata.split_ascii_whitespace();
-    let unparsed_key_str = match temp_iter.next() {
+    // find key (String) for hashmap, aka the indentifier
+    let mut ident_temp = ident_with_metadata.trim().split('"').collect::<Vec<&str>>();
+    ident_temp.retain(|&c| c != "");
+    let ident =  match ident_temp.first() {
         Some(x) => x,
-        None => panic!("error during command parsing"),
+        None => panic!("no match setting ident")
     };
-    //dbg!(&unparsed_key_str);
 
-    let unparsed_key_str_vec: Vec<&str> = unparsed_key_str.split('"').collect();
-
-    // dbg!(&unparsed_key_str_vec);
-    // unparsed_key_str_vec should still contain leading "" element
-    // and trailing ":" element, and be exactly 3 elements in length
-    if &unparsed_key_str_vec.len() == &3 {
-        // do nothing
-    } else {
-        panic!("unparsed_key_str_vec != 3")
-    }
-
-    // one more intermediate Vec
-    let mut isolated_key_str_vec: Vec<&str> = Vec::new();
-    for element in unparsed_key_str_vec {
-        if element != "" && element != ":" {
-            isolated_key_str_vec.push(element);
-        }
-    }
-
-    // check that isolated_key_str_vec has exactly 1 element
-    if &isolated_key_str_vec.len() != &1 {
-        panic!("more than one element in isolated_key_str_vec !")
-    }
-
-    let key_str = isolated_key_str_vec[0];
-    let key_str = ident;
-    //dbg!(&key_str);
-
-    // find 'keywords' in ident_with_metadata to eventually produce values
-    // that match rust types in resulting HashMap.
-    // to prevent extra detecting extra occurances, find only
-    // these 'keywords' within first set of paranthesis.
-
-    // split with an closure to support multiple 'splitters'
-    let unparsed_value_str_vec: Vec<&str> = ident_with_metadata
+    // find annotation (String) for identifier, aka values for hashmap,
+    // aka rust type 'hint'
+    let unparsed_annotation_str_vec: Vec<&str> = ident_with_metadata
         .split(|c| c == '(' || c == ')')
         .collect();
 
-    // because unparsed_value_str_vec will have an element before
+    // because unparsed_annotation_str_vec will have an element before
     // the first '(', and there may be more sets of parenthesis,
-    // only the second element with is examined with [1].
+    // only the second element is examined with [1].
     // if there are nested parenthesis this scheme will fail.
-    // TODO possibly check for nested parenthesis?
+    // TODO check for nested parenthesis?
+
+    let unparsed_annotation_str = unparsed_annotation_str_vec[1];
 
     // determine if optional.
     let mut optional: bool = false;
-    if unparsed_value_str_vec[1].contains("optional") {
+    if unparsed_annotation_str.contains("optional") {
         optional = true;
     }
+    
+    let mut annotation_str ="";
 
-    // create value collecting Vec, to then check vec
-    // has only one valid value
-    let mut value_collector_vec: Vec<&str> = Vec::new();
-
-    // transforming for HashMap, provide values to vec
-    if unparsed_value_str_vec[1].contains("numeric") {
-        value_collector_vec.push("Decimal");
+    if unparsed_annotation_str.starts_with("numeric") {
+        annotation_str = "Decimal";
     }
-    if unparsed_value_str_vec[1].contains("string") {
-        value_collector_vec.push("String");
+    if unparsed_annotation_str.starts_with("string") {
+        annotation_str = "String";
     }
-    if unparsed_value_str_vec[1].contains("boolean") {
-        value_collector_vec.push("bool");
+    if unparsed_annotation_str.starts_with("boolean") {
+        annotation_str = "bool";
     }
 
-    if &value_collector_vec.len() != &1 {
-        panic!("only 1 element allowed in value_collector_vec!")
-    }
+    if annotation_str == ""{
+        panic!("annotation_str should have a value at this point.");
+    };
 
-    let temp_value_string: String;
-    let value_str: &str;
-    // form value for Hashmap, cosidering the boolean optional
+    let temp_ann_string: String;
+    let annotation: &str;
+
     if optional {
-        temp_value_string = format!("Option<{}>", value_collector_vec[0]);
-        value_str = &temp_value_string;
+        temp_ann_string = format!("Option<{}>", annotation_str);
+        annotation = &temp_ann_string;
     } else {
-        value_str = value_collector_vec[0];
+        annotation = annotation_str;
     }
-    dbg!(value_str);
-    (key_str.to_string(), value_str.to_string())
+
+    (ident.to_string(), annotation.to_string())
 }
 #[cfg(test)]
 mod unit {
