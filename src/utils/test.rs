564904e0694690322d804d569a7b43c58bc6dd8d
dannasessha <sessha@zingolabs.com>
2020-12-07 07:48:15 +0000

    add expect_panic tests

--- src/utils/test.rs
+++ src/utils/test.rs
@@ -158,6 +158,147 @@ Result:
 }
 "#;
 
+pub const MORE_BRACKET_PAIRS_HELP_GETINFO: &str = r#"
+getinfo with two sets of curly brackets
+Result:
+{
+  "version": xxxxx,           (numeric) the server version
+  "protocolversion": xxxxx,   (numeric) the protocol version
+  "walletversion": xxxxx,     (numeric) the wallet version
+  "balance": xxxxxxx,         (numeric) the total Zcash balance of the wallet
+  "blocks": xxxxxx,           (numeric) the current number of blocks processed in the server
+  "timeoffset": xxxxx,        (numeric) the time offset (deprecated; always 0)
+}
+{
+  "connections": xxxxx,       (numeric) the number of connections
+  "proxy": "host:port",     (string, optional) the proxy used by the server
+  "difficulty": xxxxxx,       (numeric) the current difficulty
+  "testnet": true|false,      (boolean) if the server is using testnet or not
+  "keypoololdest": xxxxxx,    (numeric) the timestamp (seconds since GMT epoch) of the oldest pre-generated key in the key pool
+  "keypoolsize": xxxx,        (numeric) how many new keys are pre-generated
+  "unlocked_until": ttt,      (numeric) the timestamp in seconds since epoch (midnight Jan 1 1970 GMT) that the wallet is unlocked for transfers, or 0 if the wallet is locked
+  "paytxfee": x.xxxx,         (numeric) the transaction fee set in ZEC/kB
+  "relayfee": x.xxxx,         (numeric) minimum relay fee for non-free transactions in ZEC/kB
+  "errors": "..."           (string) any error messages
+}
+"#;
+
+pub const EXTRA_START_BRACKET_HELP_GETINFO: &str = r#"
+getinfo with two sets of curly brackets
+Result:
+{
+  "version": xxxxx,           (numeric) the server version
+  "protocolversion": xxxxx,   (numeric) the protocol version
+  "walletversion": xxxxx,     (numeric) the wallet version
+  "balance": xxxxxxx,         (numeric) the total Zcash balance of the wallet
+  "blocks": xxxxxx,           (numeric) the current number of blocks processed in the server
+  "timeoffset": xxxxx,        (numeric) the time offset (deprecated; always 0)
+{
+  "connections": xxxxx,       (numeric) the number of connections
+  "proxy": "host:port",     (string, optional) the proxy used by the server
+  "difficulty": xxxxxx,       (numeric) the current difficulty
+  "testnet": true|false,      (boolean) if the server is using testnet or not
+  "keypoololdest": xxxxxx,    (numeric) the timestamp (seconds since GMT epoch) of the oldest pre-generated key in the key pool
+  "keypoolsize": xxxx,        (numeric) how many new keys are pre-generated
+  "unlocked_until": ttt,      (numeric) the timestamp in seconds since epoch (midnight Jan 1 1970 GMT) that the wallet is unlocked for transfers, or 0 if the wallet is locked
+  "paytxfee": x.xxxx,         (numeric) the transaction fee set in ZEC/kB
+  "relayfee": x.xxxx,         (numeric) minimum relay fee for non-free transactions in ZEC/kB
+  "errors": "..."           (string) any error messages
+}
+"#;
+
+pub const EXTRA_END_BRACKET_HELP_GETINFO: &str = r#"
+getinfo with two sets of curly brackets
+Result:
+{
+  "version": xxxxx,           (numeric) the server version
+  "protocolversion": xxxxx,   (numeric) the protocol version
+  "walletversion": xxxxx,     (numeric) the wallet version
+  "balance": xxxxxxx,         (numeric) the total Zcash balance of the wallet
+  "blocks": xxxxxx,           (numeric) the current number of blocks processed in the server
+  "timeoffset": xxxxx,        (numeric) the time offset (deprecated; always 0)
+}
+  "connections": xxxxx,       (numeric) the number of connections
+  "proxy": "host:port",     (string, optional) the proxy used by the server
+  "difficulty": xxxxxx,       (numeric) the current difficulty
+  "testnet": true|false,      (boolean) if the server is using testnet or not
+  "keypoololdest": xxxxxx,    (numeric) the timestamp (seconds since GMT epoch) of the oldest pre-generated key in the key pool
+  "keypoolsize": xxxx,        (numeric) how many new keys are pre-generated
+  "unlocked_until": ttt,      (numeric) the timestamp in seconds since epoch (midnight Jan 1 1970 GMT) that the wallet is unlocked for transfers, or 0 if the wallet is locked
+  "paytxfee": x.xxxx,         (numeric) the transaction fee set in ZEC/kB
+  "relayfee": x.xxxx,         (numeric) minimum relay fee for non-free transactions in ZEC/kB
+  "errors": "..."           (string) any error messages
+}
+"#;
+
+pub const NO_RESULT_HELP_GETINFO: &str = r#"
+getinfo with no Result:
+
+{
+  "version": xxxxx,           (numeric) the server version
+  "protocolversion": xxxxx,   (numeric) the protocol version
+  "walletversion": xxxxx,     (numeric) the wallet version
+  "balance": xxxxxxx,         (numeric) the total Zcash balance of the wallet
+  "blocks": xxxxxx,           (numeric) the current number of blocks processed in the server
+  "timeoffset": xxxxx,        (numeric) the time offset (deprecated; always 0)
+  "connections": xxxxx,       (numeric) the number of connections
+  "proxy": "host:port",     (string, optional) the proxy used by the server
+  "difficulty": xxxxxx,       (numeric) the current difficulty
+  "testnet": true|false,      (boolean) if the server is using testnet or not
+  "keypoololdest": xxxxxx,    (numeric) the timestamp (seconds since GMT epoch) of the oldest pre-generated key in the key pool
+  "keypoolsize": xxxx,        (numeric) how many new keys are pre-generated
+  "unlocked_until": ttt,      (numeric) the timestamp in seconds since epoch (midnight Jan 1 1970 GMT) that the wallet is unlocked for transfers, or 0 if the wallet is locked
+  "paytxfee": x.xxxx,         (numeric) the transaction fee set in ZEC/kB
+  "relayfee": x.xxxx,         (numeric) minimum relay fee for non-free transactions in ZEC/kB
+  "errors": "..."           (string) any error messages
+}
+"#;
+pub const NO_END_BRACKET_HELP_GETINFO: &str = r#"
+getinfo with no closing bracket
+Result:
+{
+  "version": xxxxx,           (numeric) the server version
+  "protocolversion": xxxxx,   (numeric) the protocol version
+  "walletversion": xxxxx,     (numeric) the wallet version
+  "balance": xxxxxxx,         (numeric) the total Zcash balance of the wallet
+  "blocks": xxxxxx,           (numeric) the current number of blocks processed in the server
+  "timeoffset": xxxxx,        (numeric) the time offset (deprecated; always 0)
+  "connections": xxxxx,       (numeric) the number of connections
+  "proxy": "host:port",     (string, optional) the proxy used by the server
+  "difficulty": xxxxxx,       (numeric) the current difficulty
+  "testnet": true|false,      (boolean) if the server is using testnet or not
+  "keypoololdest": xxxxxx,    (numeric) the timestamp (seconds since GMT epoch) of the oldest pre-generated key in the key pool
+  "keypoolsize": xxxx,        (numeric) how many new keys are pre-generated
+  "unlocked_until": ttt,      (numeric) the timestamp in seconds since epoch (midnight Jan 1 1970 GMT) that the wallet is unlocked for transfers, or 0 if the wallet is locked
+  "paytxfee": x.xxxx,         (numeric) the transaction fee set in ZEC/kB
+  "relayfee": x.xxxx,         (numeric) minimum relay fee for non-free transactions in ZEC/kB
+  "errors": "..."           (string) any error messages
+
+"#;
+
+pub const NO_START_BRACKET_HELP_GETINFO: &str = r#"
+getinfo with no beginning bracket
+Result:
+
+  "version": xxxxx,           (numeric) the server version
+  "protocolversion": xxxxx,   (numeric) the protocol version
+  "walletversion": xxxxx,     (numeric) the wallet version
+  "balance": xxxxxxx,         (numeric) the total Zcash balance of the wallet
+  "blocks": xxxxxx,           (numeric) the current number of blocks processed in the server
+  "timeoffset": xxxxx,        (numeric) the time offset (deprecated; always 0)
+  "connections": xxxxx,       (numeric) the number of connections
+  "proxy": "host:port",     (string, optional) the proxy used by the server
+  "difficulty": xxxxxx,       (numeric) the current difficulty
+  "testnet": true|false,      (boolean) if the server is using testnet or not
+  "keypoololdest": xxxxxx,    (numeric) the timestamp (seconds since GMT epoch) of the oldest pre-generated key in the key pool
+  "keypoolsize": xxxx,        (numeric) how many new keys are pre-generated
+  "unlocked_until": ttt,      (numeric) the timestamp in seconds since epoch (midnight Jan 1 1970 GMT) that the wallet is unlocked for transfers, or 0 if the wallet is locked
+  "paytxfee": x.xxxx,         (numeric) the transaction fee set in ZEC/kB
+  "relayfee": x.xxxx,         (numeric) minimum relay fee for non-free transactions in ZEC/kB
+  "errors": "..."           (string) any error messages
+}
+"#;
+
 pub fn valid_getinfo_annotation() -> HashMap<String, String> {
     [
         ("version", "Decimal"),
