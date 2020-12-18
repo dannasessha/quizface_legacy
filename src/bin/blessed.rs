ff8f7b1f5e746975a5ba4477477991343d6d839e
dannasessha <sessha@zingolabs.com>
2020-12-18 21:35:26 +0000

    expanded blessed tests, simple rename of blessed tests

--- src/bin/blessed.rs
+++ src/bin/blessed.rs
@@ -23,7 +23,6 @@ fn blessed_check(raw_command_help: &str) -> bool {
     let delimiter_test_2: Vec<&str> =
         raw_command_help.split("Examples:\n").collect();
     if delimiter_test_1.len() != 1 && delimiter_test_2.len() != 1 {
-        // interesting test would be to put Examples first
         let split_command_help =
             raw_command_help.split("Result:\n").collect::<Vec<&str>>()[1]
                 .split("Examples:\n")
@@ -47,50 +46,58 @@ mod unit {
     use super::*;
 
     #[test]
-    fn b0() {
-        assert_eq!(true, blessed_check(BLESSED_TEST0));
+    fn b00() {
+        assert_eq!(true, blessed_check(BLESSED_TEST00));
     }
     #[test]
-    fn b1() {
-        assert_eq!(true, blessed_check(BLESSED_TEST1));
+    fn b01() {
+        assert_eq!(true, blessed_check(BLESSED_TEST01));
     }
     #[test]
-    fn b2() {
-        assert_eq!(false, blessed_check(BLESSED_TEST2));
+    fn b02() {
+        assert_eq!(false, blessed_check(BLESSED_TEST02));
     }
     #[test]
-    fn b3() {
-        assert_eq!(false, blessed_check(BLESSED_TEST3));
+    fn b03() {
+        assert_eq!(false, blessed_check(BLESSED_TEST03));
     }
     #[test]
-    fn b4() {
-        assert_eq!(false, blessed_check(BLESSED_TEST4));
+    fn b04() {
+        assert_eq!(false, blessed_check(BLESSED_TEST04));
     }
     #[test]
-    fn b5() {
-        assert_eq!(false, blessed_check(BLESSED_TEST5));
+    fn b05() {
+        assert_eq!(false, blessed_check(BLESSED_TEST05));
     }
     #[test]
-    fn b6() {
-        assert_eq!(false, blessed_check(BLESSED_TEST6));
+    fn b06() {
+        assert_eq!(false, blessed_check(BLESSED_TEST06));
     }
     #[test]
-    fn b7() {
-        assert_eq!(false, blessed_check(BLESSED_TEST7));
+    fn b07() {
+        assert_eq!(false, blessed_check(BLESSED_TEST07));
     }
     #[test]
-    fn b8() {
-        assert_eq!(false, blessed_check(BLESSED_TEST8));
+    fn b08() {
+        assert_eq!(false, blessed_check(BLESSED_TEST08));
+    }
+    #[test]
+    fn b09() {
+        assert_eq!(false, blessed_check(BLESSED_TEST09));
+    }
+    #[test]
+    fn b10() {
+        assert_eq!(false, blessed_check(BLESSED_TEST10));
     }
     
-    pub const BLESSED_TEST0: &str = r#"
+    pub const BLESSED_TEST00: &str = r#"
 There are one of each delimiter.
 Result:
 There is not only whitespace between delimiters.
 Examples:
 This should pass.
 "#;
-    pub const BLESSED_TEST1: &str = r#"
+    pub const BLESSED_TEST01: &str = r#"
 This should also pass.
 Even though there is an extra Result:  it is not on its own line
 Result:
@@ -98,39 +105,52 @@ There is not only whitespace between delimiters.
 Examples:
 and the same is true for an extra Examples: not on its own line
 "#;
-    pub const BLESSED_TEST2: &str = r#"
+    pub const BLESSED_TEST02: &str = r#"
 This should fail, as well as all subsequent BLESSED_TESTs.
 There is only whitespace between delimiters, with text before.
 Result:
 Examples:
 "#;
-    pub const BLESSED_TEST3: &str = r#"
+    pub const BLESSED_TEST03: &str = r#"
 Result:
 Examples:
 There is only whitespace between delimiters, with text after.
-All further
+All further text doesn't
+effect anything.
+...
 "#;
-    pub const BLESSED_TEST4: &str = r#"
+    pub const BLESSED_TEST04: &str = r#"
 Delimiters in the wrong order.
 Examples:
 Result:
 "#;
-    pub const BLESSED_TEST5: &str = r#"
+    pub const BLESSED_TEST05: &str = r#"
 Only one delimiter.
 Examples:
 "#;
-    pub const BLESSED_TEST6: &str = r#"
+    pub const BLESSED_TEST06: &str = r#"
 Only one delimiter.
 Result:
 "#;
-    pub const BLESSED_TEST7: &str = r#"
+    pub const BLESSED_TEST07: &str = r#"
 More than one of the same delimiter, Result:.
 Result:
 Result:
 "#;
-    pub const BLESSED_TEST8: &str = r#"
+    pub const BLESSED_TEST08: &str = r#"
 More than one of the same delimiter, Examples:.
 Examples:
+Examples:
+"#;
+    pub const BLESSED_TEST09: &str = r#"
+Delimiters in wrong order with text between them:
+Examples:
+some text here doesn't change anything; not blessed.
+Result:
+"#;
+    pub const BLESSED_TEST10: &str = r#"
+Seeing `bResult:` on its own line fails.
+bResult:
 Examples:
 "#;
 }
