0fc176174936f5343d16e666e34d53403365be60
Dan Nases Sha <70554613+dannasessha@users.noreply.github.com>
2021-02-20 08:34:15 +0000

    Revert "Log json interpretation"

--- src/utils/logging.rs
+++ src/utils/logging.rs
@@ -2,9 +2,21 @@ use std::fs;
 use std::path::Path;
 const QUIZFACE_VERSION: &'static str = env!("CARGO_PKG_VERSION");
 
-fn name_logdirs() -> (String, String, String) {
-    let log_parent_template: String =
-        format!("./logs/{}/", create_version_name());
+pub fn create_log_dirs() {
+    fs::create_dir_all(Path::new(&name_logdirs().0))
+        .expect("error crating master dir!");
+    fs::create_dir_all(Path::new(&name_logdirs().1))
+        .expect("error creating commands dir!");
+    fs::create_dir_all(Path::new(&name_logdirs().2))
+        .expect("error creating blessed dir!");
+}
+
+pub fn name_logdirs() -> (String, String, String) {
+    let log_parent_template: String = format!(
+        "./logs/{zdver}_{qfver}/",
+        zdver = get_zcashd_version(),
+        qfver = QUIZFACE_VERSION
+    );
     let master_name: String =
         format!("{}masterhelp_output/raw/", log_parent_template);
     let base_name: String = format!("{}help_output/raw/", log_parent_template);
@@ -13,7 +25,7 @@ fn name_logdirs() -> (String, String, String) {
     (master_name, base_name, blessed_name)
 }
 
-fn get_zcashd_version() -> String {
+pub fn get_zcashd_version() -> String {
     let version = std::process::Command::new("zcash-cli")
         .arg("--version")
         .output()
@@ -30,25 +42,12 @@ fn get_zcashd_version() -> String {
         .to_string()
 }
 
-pub(crate) fn create_version_name() -> String {
-    format!("{}_{}", get_zcashd_version(), QUIZFACE_VERSION)
-}
-
-pub fn create_log_dirs() {
-    fs::create_dir_all(Path::new(&name_logdirs().0))
-        .expect("error creating master dir!");
-    fs::create_dir_all(Path::new(&name_logdirs().1))
-        .expect("error creating commands dir!");
-    fs::create_dir_all(Path::new(&name_logdirs().2))
-        .expect("error creating blessed dir!");
-}
-
 pub fn log_masterhelp_output(raw_help: &str) {
     fs::write(format!("{}masterhelp.txt", name_logdirs().0), raw_help)
         .expect("panic during fs:write masterhelp!");
 }
 
-pub fn log_raw_output(command: &str, raw_command_help: String) {
+pub fn log_raw_output(command: String, raw_command_help: String) {
     fs::write(
         format!("{}{}.txt", name_logdirs().1, &command),
         &raw_command_help,
